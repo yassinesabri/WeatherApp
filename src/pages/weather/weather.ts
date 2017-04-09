313c6188f4516de4fa249d6114157eeb3b310b66
YASSINE SABRI <yassine.sabri@edu.uca.ma>
2017-04-09 16:33:59 +0100

    display data on the main page

--- src/pages/weather/weather.ts
+++ src/pages/weather/weather.ts
@@ -13,13 +13,15 @@ import {WeatherService} from '../../services/weather.service';
 export class WeatherPage implements OnInit{
   city:string;
   state:string;
+  weather:any;
   constructor(public navCtrl: NavController,public weatherService:WeatherService) {
     this.city='BOSTON';
     this.state='MA';
   }
   ngOnInit(){
     this.weatherService.getWeather(this.city,this.state).subscribe(data => {
-      console.log(data);
+      //console.log(data);
+      this.weather = data.current_observation;
     });
 
   }
